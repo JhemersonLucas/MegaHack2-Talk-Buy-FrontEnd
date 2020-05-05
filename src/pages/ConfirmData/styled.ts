41a9e28b5aea6ab42f4538ee380cf0a17a9c267a
Jhemerson Lucas <jhemersonlucas@hotmail.com>
2020-05-04 23:22:42 -0300

    ajustes pos checkout

--- src/pages/ConfirmData/styled.ts
+++ src/pages/ConfirmData/styled.ts
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 80vw;
+  max-width: 700px;
+  width: 100%;
+  padding: 10px 40px;
   text-align: center;
 `;
 
 export const DataContainer = styled.div`
   background-color: #fff;
-  padding: 40px;
+  padding: 20px;
+  margin: 0 20px;
   border-radius: 30px;
   display: flex;
   flex-direction: column;
@@ -18,7 +21,7 @@ export const DataContainer = styled.div`
 
 export const Title = styled.div`
   margin-top: 50px;
-  font-size: 2em;
+  font-size: 1.8em;
   line-height: 40px;
   font-weight: 600;
   text-align: center;
@@ -28,4 +31,9 @@ export const Data = styled.p`
   color: #000;
   font-size: 1.2em;
   margin-bottom: 15px;
+
+  @media (max-width: 550px) {
+    font-size: 1em;
+    line-height: 30px;
+  }
 `;
